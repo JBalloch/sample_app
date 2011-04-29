a104b465073ed0228daffc55078e1e40e122916d
James G. Balloch <jamesgeorgeballoch@gmail.com>
2011-04-29 14:17:47 +0100

    Finished layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   root :to => "pages#home"
-		
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+
+  match '/signup', :to => 'users#new'
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
