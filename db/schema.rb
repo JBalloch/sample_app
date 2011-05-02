92c2f95beb2756f985df4aebdf980f10d9ccd3e0
James G. Balloch <jamesgeorgeballoch@gmail.com>
2011-05-02 12:52:29 +0100

    User model with passwords, and the user show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110429164837) do
+ActiveRecord::Schema.define(:version => 20110501163305) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,46 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
+  
+  before(:each) do
+    @attr = {
+      :name => "Other User",
+      :email => "other@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
+    @user = User.create!(@attr)
+  end
 
-  describe "GET 'new'" do
+  describe "GET 'show'" do
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+    
+  end  
+
+  describe "GET 'new'"
     it "should be successful" do
       get 'new'
       response.should be_success
@@ -14,4 +52,3 @@ describe UsersController do
       response.should have_selector("title", :content => "Sign up")
     end
   end
-end
